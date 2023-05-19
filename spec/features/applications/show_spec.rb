a69cbcee4f9a3318e9b36be6f11a372b643163b1
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-19 17:15:32 -0400

    fix pet shelter association, delete shelter id

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "the applications show page" do
 
   let!(:shelter) { Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9) }
-  let!(:pet) { shelter.pets.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id) }
+  let!(:pet) { shelter.pets.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true) }
   let!(:application) { Application.create!(name: "Ringo Starr", street_address: "123 Canyon Blvd.", city: "Boulder", state: "CO", zip_code: "80304", description: "I just love pets so much!", status: "In Progress") }
 
   it "shows the application and its attributes" do
