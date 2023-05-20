30ef3fae21a05161010d23146356b40b51927be0
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-20 16:11:37 -0400

    add search feature to find pets associated with an input

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,6 +2,10 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:app_id])
     @pets = @application.pets
+
+    if params[:search]
+      @found_pets = Pet.search(params[:search])
+    end
   end
 
   def new
