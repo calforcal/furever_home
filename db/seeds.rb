f7e50d1528005a0e32a29bc1564e3ff6d77a9e52
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-21 15:49:00 -0400

    add more pet seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,10 @@ PetApplication.destroy_all
 shelter = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
 
 pet   = Pet.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
-pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter.id)
-pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter.id)
+pet_1 = Pet.create!(name: "Lucille Bald", breed: "sphynx", adoptable: true, age: 1, shelter_id: shelter.id)
+pet_2 = Pet.create!(name: "Lobster", breed: "doberman", adoptable: true, age: 3,  shelter_id: shelter.id)
+pet_3 = Pet.create!(name: "Scooter", breed: "Poodle", adoptable: true, age: 10,  shelter_id: shelter.id)
+pet_4 = Pet.create!(name: "Sir Scoots", breed: "Mix", adoptable: true, age: 4,  shelter_id: shelter.id)
 
 application = Application.create!(name: "Ringo Starr", street_address: "123 Canyon Blvd.", city: "Boulder", state: "CO", zip_code: "80304", description: "I just love pets so much!", status: "In Progress")
 
