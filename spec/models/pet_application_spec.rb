407f0f6d9508555f1031b1a21bb195a6b1955dd3
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-24 17:10:33 -0400

    add validations andtests to pet app model

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -3,4 +3,8 @@ require "rails_helper"
 RSpec.describe PetApplication, type: :model do
   it { should belong_to :pet }
   it { should belong_to :application }
+
+  it { should validate_presence_of :pet_status}
+  it { should validate_presence_of :pet_id}
+  it { should validate_presence_of :application_id}
 end
