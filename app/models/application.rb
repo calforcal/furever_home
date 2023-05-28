25f8127bbcc69d667238ce61aab38ca2af9f41ae
Michael Callahan <mcalla242@gmail.com>
2023-05-28 10:45:54 -0600

    Add feature and test specs to update application status when all pets decided on

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,4 +7,11 @@ class Application < ApplicationRecord
   def find_pet_app(pet_id)
     pet_applications.where(pet_id: pet_id).first
   end
+
+  def update_status
+    value = self.pet_applications.where(pet_status: 'Pending Adoption').or(self.pet_applications.where(pet_status: 'Rejected')).any?
+    if value != true
+      self.update(status: "Approved")
+    end
+  end
 end
